875bea0814348393c6e31c51b96a989caedf6294
dana-alsibai <alsibaid@sheridancollege.ca>
2020-06-13 21:58:50 -0400

    Revel Book Exercise02_22

---
+++ src/exercise02_22/Exercise02_22.java
@@ -0,0 +1,26 @@
+  /*
+        Name:  [your name here]
+        Assignment:  [assignment name]
+        Program: [your program name here]
+        Date:  [assignment due date here]
+    
+        Description:
+        [program description in your own words]
+    */
+
+package exercise02_22;
+
+/**
+ *
+ * @author danasebai
+ */
+public class Exercise02_22 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
